7660814c06a27e090ab8aba296028060cdfd949c
drmorrisontierp <drmorrisontierp@gmail.com>
2025-01-12 10:11:37 +0100

    third

--- script.js
+++ script.js
@@ -75,61 +75,182 @@ function generateAddition() {
   for (let x = 0; x < 4; x++) {
     numbers[0].push((Math.round(Math.random() * 9)).toString())
   }
-  for (let x = 0; x < 3; x++) {
+  for (let x = 0; x < 4; x++) {
     numbers[1].push((Math.round(Math.random() * 9)).toString())
   }
   numbers[0][Math.round(Math.random() * 2 + 1)] = "."
-  numbers[1][Math.round(Math.random() * 1 + 1)] = "."
+  numbers[1][Math.round(Math.random() * 2 + 1)] = "."
   return numbers
 }
 
-function checkNumbers(numbers) {
-  let number_one = numbers[0]
-  let number_two = numbers[1]
+function reduceNumbers(number) {
+  let decX = 2
 
-  let x = number_one.indexOf(".")
-  let y = number_two.indexOf(".")
-  let flag = true
-
-  if ((x == 2 || x == 3) && number_one[0] == "0") number_one.shift()
-  if (x == 3 && number_one[0] == "0") number_one.shift()
-  if (y == 2 && number_two[0] == "0") number_two.shift()
-  if (x == 1 && number_one[number_one.length - 1] == "0") {
-    number_one.pop()
-    if (number_one[number_one.length - 1] == "0") {
-      number_one.pop()
-      number_one.pop()
+  let x = number.indexOf(".")
+  if ((x == 2) && number[0] == "0") {
+    number.shift()
+    decX = 2
+  }
+  if ((x == 3) && number[0] == "0") {
+    number.shift()
+    decX = 1
+  }
+  if (x == 1 && number[number.length - 1] == "0") {
+    number.pop()
+    decX = 2
+    if (number[number.length - 1] == "0") {
+      number.pop()
+      decX = 1
+      if (number[number.length - 1] == "0") {
+        number.pop()
+        number.pop()
+        decX = 0
+      }
     }
   }
-  if (x == 2 && number_one[number_one.length - 1] == "0") {
-    number_one.pop()
-    number_one.pop()
+  if (x == 2 && number[number.length - 1] == "0") {
+    number.pop()
+    decX = 1
+    if (number[number.length - 1] == "0") {
+      number.pop()
+      number.pop()
+      decX = 0
+    }
   }
-  if (x == 3) number_one.pop()
-  if (y == 2) number_two.pop()
-  if (y == 1 && number_two[number_two.length - 1] == "0") {
-    number_two.pop()
-    number_two.pop()
+
+  if (x == 3) {
+    number.pop()
+    decX = 0
   }
-  if (parseFloat(number_one.join("")) == 0 || parseFloat(number_two.join("")) == 0) flag = false
-  return [x, y, [number_one, number_two], flag] 
+  return [decX, number]
 }
 
+function checkNumbers(numbers) {
+  let flag = true;
+  let number_one = reduceNumbers(numbers[0]);
+  let number_two = reduceNumbers(numbers[1]);
 
+  if (parseFloat(number_one[1].join("")) == 0 || parseFloat(number_two[1].join("")) == 0) flag = false;
+
+  let x = number_one[1].indexOf(".");
+  let y = number_two[1].indexOf(".");
+
+  if (number_one[0] === number_two[0]) {
+    while (number_one[1].length < 5) {
+      number_one[1].unshift("");
+    }
+    while (number_two[1].length < 5) {
+      number_two[1].unshift("");
+    }
+  } else if (number_one[0] > number_two[0]) {
+    while (number_one[1].length < 5) {
+      number_one[1].unshift("");
+    }
+    while (number_two[1].length < 5 + (number_one[0] - number_two[0])) {
+      number_two[1].unshift("");
+    }
+  } else {
+    while (number_two[1].length < 5) {
+      number_two[1].unshift("");
+    }
+    while (number_one[1].length < 5 + (number_two[0] - number_one[0])) {
+      number_one[1].unshift("");
+    }
+  }
+
+  return [number_one[1].concat(number_two[1]), flag];
+}
+/*
+function checkNumbers(numbers) {
+
+  let flag = true
+  let number_one = reduceNumbers(numbers[0])
+  let number_two = reduceNumbers(numbers[1])
+
+  if (parseFloat(number_one[1].join("")) == 0 || parseFloat(number_two[1].join("")) == 0) flag = false
+
+  x = number_one[1].indexOf(".");
+  y = number_two[1].indexOf(".");
+
+  if (number_one[0] == 0 && number_two[0] == 0) {
+    for (let x = 0; x < 5 - number_one[1].length; x++) {
+      number_one[1].unshift("")
+    }
+    for (let x = 0; x < 5 - number_two[1].length; x++) {
+      number_two[1].unshift("")
+    }
+  }
+  if (number_one[0] === number_two[0]) {
+    for (let x = 0; x < 5 - number_one[1].length; x++) {
+      number_one[1].unshift("")
+    }
+    for (let x = 0; x < 5 - number_two[1].length; x++) {
+      number_two[1].unshift("")
+    }
+  }
+  if (number_one[0] > number_two[0]) {
+    for (let x = 0; x < 5 - number_one[1].length; x++) {
+      number_one[1].unshift("")
+    }
+    for (let x = 0; x < (number_one[0] - number_two[0]); x++) {
+      number_two[1].push("")
+    }
+    if (number_two[0] == 0) number_two[1].push("")
+
+    for (let x = 0; x < 5 - number_two[1].length; x++) {
+      number_two[1].unshift("")
+    }
+
+  }
+  if (number_two[0] > number_one[0]) {
+    for (let x = 0; x < 5 - number_one.length[1]; x++) {
+      number_two[1].unshift("")
+    }
+    for (let x = 0; x < (number_two[0] - number_one[0]); x++) {
+      number_one[1].push("")
+    }
+    if (number_one[0] == 0) number_one[1].push("")
+    for (let x = 0; x < 5 - number_two[1].length; x++) {
+      number_one[1].unshift("")
+    }
+
+  }
+
+  return [number_one[1].concat(number_two[1]), flag]
+}
+*/
+function placeNumbers(numbers) {
+  let placings = {
+    'd11': "",
+    'd13': "",
+    'd15': "",
+    'd17': "",
+    'd19': "",
+    'd21': "",
+    'd23': "",
+    'd25': "",
+    'd27': "",
+    'd29': "",
+  }
+  for (let x = 0; x < 10; x++) {
+    placings[`d${10 + 2 * x + 1}`] = numbers[0][x]
+  }
+
+}
 
 let numbers = generateAddition()
 //let numbers = [["0", "0", ".", "0"], ["0", ".", "0"]]
-console.log(numbers)
+//console.log(numbers)
 let numberCheck = checkNumbers(numbers)
 console.log(numberCheck)
-let flag = numberCheck[3]
+let flag = numberCheck[1]
 while (!flag) {
   numbers = generateAddition()
   numberCheck = checkNumbers(numbers)
-  flag = numberCheck[3]
+  flag = numberCheck[1]
 }
-console.log(numberCheck)
-
+//console.log(numberCheck)
+placeNumbers(numberCheck)
 
 /*const test = [
   [["0", "0", ".", "0"], ["0", ".", "0"]],
