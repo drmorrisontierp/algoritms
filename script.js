13cc6a606d899c31c8316ae3744818bdaa4a89d8
drmorrisontierp <drmorrisontierp@gmail.com>
2025-01-12 14:30:10 +0100

    fourth

--- script.js
+++ script.js
@@ -1,59 +1,67 @@
 
 const ids = [
+  'd10',
   'd11',
+  'd12',
   'd13',
+  'd14',
   'd15',
-  'd17',
-  'd19',
+  'd20',
   'd21',
+  'd22',
   'd23',
+  'd24',
   'd25',
-  'd27',
-  'd29',
+  'b10',
   'b11',
+  'b12',
   'b13',
+  'b14',
   'b15',
-  'b17',
-  'b19',
+  'b20',
   'b21',
+  'b22',
   'b23',
+  'b24',
   'b25',
-  'b27',
-  'b29',
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
   const dict = {
+    d10: document.getElementById('d10'),
     d11: document.getElementById('d11'),
+    d12: document.getElementById('d12'),
     d13: document.getElementById('d13'),
+    d14: document.getElementById('d14'),
     d15: document.getElementById('d15'),
-    d17: document.getElementById('d17'),
-    d19: document.getElementById('d19'),
+    d20: document.getElementById('d20'),
     d21: document.getElementById('d21'),
+    d22: document.getElementById('d22'),
     d23: document.getElementById('d23'),
+    d24: document.getElementById('d24'),
     d25: document.getElementById('d25'),
-    d27: document.getElementById('d27'),
-    d29: document.getElementById('d29'),
+    b10: document.getElementById('b10'),
     b11: document.getElementById('b11'),
+    b12: document.getElementById('b12'),
     b13: document.getElementById('b13'),
+    b14: document.getElementById('b14'),
     b15: document.getElementById('b15'),
-    b17: document.getElementById('b17'),
-    b19: document.getElementById('b19'),
+    b20: document.getElementById('b20'),
     b21: document.getElementById('b21'),
+    b22: document.getElementById('b22'),
     b23: document.getElementById('b23'),
+    b24: document.getElementById('b24'),
     b25: document.getElementById('b25'),
-    b27: document.getElementById('b27'),
-    b29: document.getElementById('b29'),
   };
 
-  for (let x = 0; x < 10; x++) {
+  for (let x = 0; x < 12; x++) {
     dict[ids[x]].addEventListener('dragstart', function (e) {
       console.log("dragging")
       e.dataTransfer.setData('text/plain', ids[x]);
     });
   }
 
-  for (let x = 10; x < 20; x++) {
+  for (let x = 12; x < 24; x++) {
     dict[ids[x]].addEventListener('dragover', function (e) {
       console.log("dragging")
       e.preventDefault();
@@ -84,31 +92,15 @@ function generateAddition() {
 }
 
 function reduceNumbers(number) {
-  let decX = 2
-
   let x = number.indexOf(".")
+  let decX = x == 1 ? 2 : x == 2 ? 1 : 0
   if ((x == 2) && number[0] == "0") {
     number.shift()
-    decX = 2
   }
   if ((x == 3) && number[0] == "0") {
     number.shift()
-    decX = 1
   }
   if (x == 1 && number[number.length - 1] == "0") {
-    number.pop()
-    decX = 2
-    if (number[number.length - 1] == "0") {
-      number.pop()
-      decX = 1
-      if (number[number.length - 1] == "0") {
-        number.pop()
-        number.pop()
-        decX = 0
-      }
-    }
-  }
-  if (x == 2 && number[number.length - 1] == "0") {
     number.pop()
     decX = 1
     if (number[number.length - 1] == "0") {
@@ -116,6 +108,12 @@ function reduceNumbers(number) {
       number.pop()
       decX = 0
     }
+  }
+
+  if (x == 2 && number[number.length - 1] == "0") {
+    number.pop()
+    number.pop()
+    decX = 0
   }
 
   if (x == 3) {
@@ -129,113 +127,77 @@ function checkNumbers(numbers) {
   let flag = true;
   let number_one = reduceNumbers(numbers[0]);
   let number_two = reduceNumbers(numbers[1]);
+  console.log(number_one)
+  console.log(number_two)
 
   if (parseFloat(number_one[1].join("")) == 0 || parseFloat(number_two[1].join("")) == 0) flag = false;
 
-  let x = number_one[1].indexOf(".");
-  let y = number_two[1].indexOf(".");
-
   if (number_one[0] === number_two[0]) {
-    while (number_one[1].length < 5) {
+    while (number_one[1].length < 6) {
       number_one[1].unshift("");
     }
-    while (number_two[1].length < 5) {
+    while (number_two[1].length < 6) {
       number_two[1].unshift("");
     }
   } else if (number_one[0] > number_two[0]) {
-    while (number_one[1].length < 5) {
+    let extraDecs = number_two[0] !== 0 ? number_one[0] - number_two[0] : number_one[0] - number_two[0] + 1;
+    for (let x = 0; x < extraDecs; x++) {
+      number_two[1].push("");
+    }
+    while (number_one[1].length < 6) {
       number_one[1].unshift("");
     }
-    while (number_two[1].length < 5 + (number_one[0] - number_two[0])) {
+    while (number_two[1].length < 6) {
       number_two[1].unshift("");
     }
   } else {
-    while (number_two[1].length < 5) {
+    let extraDecs = number_one[0] !== 0 ? number_two[0] - number_one[0] : number_two[0] - number_one[0] + 1;
+    for (let x = 0; x < extraDecs; x++) {
+      number_one[1].push("");
+    }
+    while (number_two[1].length < 6) {
       number_two[1].unshift("");
     }
-    while (number_one[1].length < 5 + (number_two[0] - number_one[0])) {
+    while (number_one[1].length < 6) {
       number_one[1].unshift("");
     }
   }
-
+  
+  console.log(number_one)
+  console.log(number_two)
   return [number_one[1].concat(number_two[1]), flag];
 }
-/*
-function checkNumbers(numbers) {
 
-  let flag = true
-  let number_one = reduceNumbers(numbers[0])
-  let number_two = reduceNumbers(numbers[1])
-
-  if (parseFloat(number_one[1].join("")) == 0 || parseFloat(number_two[1].join("")) == 0) flag = false
-
-  x = number_one[1].indexOf(".");
-  y = number_two[1].indexOf(".");
-
-  if (number_one[0] == 0 && number_two[0] == 0) {
-    for (let x = 0; x < 5 - number_one[1].length; x++) {
-      number_one[1].unshift("")
-    }
-    for (let x = 0; x < 5 - number_two[1].length; x++) {
-      number_two[1].unshift("")
-    }
-  }
-  if (number_one[0] === number_two[0]) {
-    for (let x = 0; x < 5 - number_one[1].length; x++) {
-      number_one[1].unshift("")
-    }
-    for (let x = 0; x < 5 - number_two[1].length; x++) {
-      number_two[1].unshift("")
-    }
-  }
-  if (number_one[0] > number_two[0]) {
-    for (let x = 0; x < 5 - number_one[1].length; x++) {
-      number_one[1].unshift("")
-    }
-    for (let x = 0; x < (number_one[0] - number_two[0]); x++) {
-      number_two[1].push("")
-    }
-    if (number_two[0] == 0) number_two[1].push("")
-
-    for (let x = 0; x < 5 - number_two[1].length; x++) {
-      number_two[1].unshift("")
-    }
-
-  }
-  if (number_two[0] > number_one[0]) {
-    for (let x = 0; x < 5 - number_one.length[1]; x++) {
-      number_two[1].unshift("")
-    }
-    for (let x = 0; x < (number_two[0] - number_one[0]); x++) {
-      number_one[1].push("")
-    }
-    if (number_one[0] == 0) number_one[1].push("")
-    for (let x = 0; x < 5 - number_two[1].length; x++) {
-      number_one[1].unshift("")
-    }
-
-  }
-
-  return [number_one[1].concat(number_two[1]), flag]
-}
-*/
 function placeNumbers(numbers) {
   let placings = {
+    'd10': "",
     'd11': "",
+    'd12': "",
     'd13': "",
+    'd14': "",
     'd15': "",
-    'd17': "",
-    'd19': "",
+    'd20': "",
     'd21': "",
+    'd22': "",
     'd23': "",
+    'd24': "",
     'd25': "",
-    'd27': "",
-    'd29': "",
   }
-  for (let x = 0; x < 10; x++) {
-    placings[`d${10 + 2 * x + 1}`] = numbers[0][x]
+  for (let x = 0; x < 6; x++) {
+    placings[`d${x + 10}`] = numbers[0][x]
   }
+  for (let x = 6; x < 12; x++) {
+    placings[`d${x + 14}`] = numbers[0][x]
+  }
+  console.log(placings)
+  return placings
+}
 
+function writeNumbers(numbers) {
+  Object.keys(numbers)
+  .forEach(function eachKey(key) {
+    document.getElementById(key).innerHTML = `<p>${numbers[key]}</p>`
+  });
 }
 
 let numbers = generateAddition()
@@ -250,7 +212,7 @@ while (!flag) {
   flag = numberCheck[1]
 }
 //console.log(numberCheck)
-placeNumbers(numberCheck)
+writeNumbers(placeNumbers(numberCheck))
 
 /*const test = [
   [["0", "0", ".", "0"], ["0", ".", "0"]],
